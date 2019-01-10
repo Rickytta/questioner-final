14fcb40b6296539852203019c1fdd0ac4b9881c3
Cop1fab <bienaimecopain@gmail.com>
2019-01-11 00:00:18 +0200

    [chore #163022499] setup models

---
+++ src/models/meetups.js
@@ -0,0 +1,21 @@
+const meetups = [{
+    id: 1,
+    createdOn: 1547116328361,
+    location: 'Kigali Convention Center',
+    images: ['../imagebank/css/css.jpg', 'http://imagebank/css/meetup7.jpg'],
+    topic: 'Web designing without CSS framework',
+    happeningOn: 'December 12, 2018',
+    tags: ['css', 'web', 'kigali']
+  },
+  {
+    id: 2,
+    createdOn: 1547116328361,
+    location: 'Kigali Institute of Science and technology',
+    images: './assets/images/dev.jpg',
+    topic: 'Test Driven Development',
+    happeningOn: 'January 27, 2019',
+    tags: ['TDD', 'OOP', 'Javascript']
+  }
+];
+
+export default meetups;
---
+++ src/models/rsvps.js
@@ -0,0 +1,14 @@
+const rsvps = [{
+    id: 1,
+    meetup: 1,
+    user: 1,
+    response: "yes"
+  },
+  {
+    id: 2,
+    meetup: 2,
+    user: 2,
+    response: "Maybe"
+  }
+]
+export default rsvps;
