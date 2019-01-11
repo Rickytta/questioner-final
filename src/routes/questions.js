f8bf6ab0f920eecbd402cb114918522ad1cbdeb2
Cop1fab <bienaimecopain@gmail.com>
2019-01-11 10:31:14 +0200

    [feature-#163142452] added endpoint allowing admin to get all questions

--- src/routes/questions.js
+++ src/routes/questions.js
@@ -5,6 +5,8 @@ import Question from '../controllers/Question';
 const router = express.Router();
 
 router.post('/', Question.create);
+router.get('/', Question.getAllQuestions);
+
 
 
 export default router;
