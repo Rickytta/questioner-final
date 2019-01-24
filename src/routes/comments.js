35c37612f3375528ab46d0b5a7ec8999440ed348
Cop1fab <bienaimecopain@gmail.com>
2019-01-24 13:00:26 +0200

    [chore-163449614] added auth for comments

--- src/routes/comments.js
+++ src/routes/comments.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import Comment from '../controllers/Comment';
+import verifyToken from '../middlewares/verifyToken';
 
 const router = express.Router();
 
-router.get('/', Comment.getAllComments);
-router.get('/:commentId', Comment.getComment);
-router.delete('/:commentId', Comment.deleteComment);
+router.get('/', verifyToken, Comment.getAllComments);
+router.get('/:commentId', verifyToken, Comment.getComment);
+router.delete('/:commentId', verifyToken, Comment.deleteComment);
 
 export default router;
