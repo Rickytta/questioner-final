35408d8c42179748a1dbbee485593f490feec014
Cop1fab <bienaimecopain@gmail.com>
2019-01-25 03:15:07 +0200

    [chore-#163470429] added tests

--- src/tests/meetups.js
+++ src/tests/meetups.js
@@ -1,5 +1,6 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
+import db from '../models/db';
 import app from '../app';
 
 const {
@@ -7,46 +8,41 @@ const {
 } = chai;
 
 chai.use(chaiHttp);
+let token = '';
 
 describe('meetups', () => {
-
-  /* Get upcoming meetups */
-  describe('GET /api/v1/meetups/upcoming', () => {
-    it('it should return an array of upcoming meetups', (done) => {
-      chai.request(app).get('/api/v1/meetups/upcoming').end((err, res) => {
-        expect(res.status).to.equal(200);
-        expect(res.body.data.length).to.be.above(0);
-        done();
-      });
-    }); // end of test
+  // clear meetups table
+  before(async () => {
+    try {
+      await db.query('TRUNCATE meetups CASCADE; ALTER SEQUENCE meetups_id_seq RESTART WITH 1;');
+    } catch (error) {
+      console.log(error);
+    }
   });
 
-  /* Get all meetups */
-  describe('GET /api/v1/meetups', () => {
-    it('it should return an array of meetups', (done) => {
-      chai.request(app).get('/api/v1/meetups').end((err, res) => {
-        expect(res.status).to.equal(200);
-        expect(res.body.data.length).to.be.above(0);
-        done();
-      });
-    }); // end of test
-  });
-
-  /* Get a specific meetup */
-  describe('GET /api/v1/meetups/:meetupId', () => {
-    it('it should get a specific meetup', (done) => {
-      chai.request(app).get('/api/v1/meetups/1').end((err, res) => {
-        expect(res.status).to.equal(200);
-        expect(Object.keys(res.body.data).length).to.be.above(0);
-        done();
-      }); // end of test
+  describe('POST /api/v1/auth/login', () => {
+    it('should return the user information if the account exists', (done) => {
+      chai.request(app)
+        .post('/api/v1/auth/login')
+        .send({
+          email: 'jonathanrwabahizi@gmail.com',
+          password: '12345',
+        })
+        .end((err, res) => {
+          expect(res.status).to.equal(200);
+          expect(Object.keys(res.body.data).length).to.be.above(0);
+          token = res.body.token;
+          done();
+        });
     });
   });
-
   /* Create a meetup */
   describe('POST /api/v1/meetups', () => {
     it('it should create a meetup', (done) => {
-      chai.request(app).post('/api/v1/meetups').send({
+      chai.request(app)
+        .post('/api/v1/meetups')
+        .set('access-token', token)
+        .send({
           location: 'Kigali Convention Center',
           images: ['../imagebank/css/css.jpg', 'http://imagebank/css/meetup7.jpg'],
           topic: 'Web designing without CSS framework',
@@ -61,23 +57,57 @@ describe('meetups', () => {
     }); // end of test
   });
 
-  /* Delete a specific meetup */
-  describe('DELETE /api/v1/:meetupId', () => {
-    it('it should delete an existing meetup', (done) => {
-      chai.request(app).delete('/api/v1/meetups/2').end((err, res) => {
-        expect(res.status).to.equal(200);
-        expect(res.body.data.toLowerCase()).to.be.equal('meetup deleted');
-        done();
-      });
+  /* Get upcoming meetups */
+  describe('GET /api/v1/meetups/upcoming', () => {
+    it('it should return an array of upcoming meetups', (done) => {
+      chai.request(app)
+        .get('/api/v1/meetups/upcoming')
+        .set('access-token', token)
+        .end((err, res) => {
+          expect(res.status).to.equal(200);
+          expect(res.body.data.length).to.be.above(0);
+          done();
+        });
+    }); // end of test
+  });
+
+  /* Get all meetups */
+  describe('GET /api/v1/meetups', () => {
+    it('it should return an array of meetups', (done) => {
+      chai.request(app)
+        .get('/api/v1/meetups')
+        .set('access-token', token)
+        .end((err, res) => {
+          expect(res.status).to.equal(200);
+          expect(res.body.data.length).to.be.above(0);
+          done();
+        });
+    }); // end of test
+  });
+
+  /* Get a specific meetup */
+  describe('GET /api/v1/meetups/:meetupId', () => {
+    it('it should get a specific meetup', (done) => {
+      chai.request(app)
+        .get('/api/v1/meetups/1')
+        .set('access-token', token)
+        .end((err, res) => {
+          expect(res.status).to.equal(200);
+          expect(Object.keys(res.body.data).length).to.be.above(0);
+          done();
+        }); // end of test
     });
-  }); //end of test
+  });
 
   /* Create a reservation*/
   describe('POST /api/v1/meetups/:meetupId/rsvps', () => {
     it('it should CREATE a reservation for a specific meetup', (done) => {
-      chai.request(app).post('/api/v1/meetups/1/rsvps').send({
-          user: 2,
-          response: "yes"
+      chai.request(app)
+        .post('/api/v1/meetups/1/rsvps')
+        .set('access-token', token)
+        .send({
+          createdBy: 1,
+          response: 'yes'
         })
         .end((err, res) => {
           expect(res.status).to.equal(201);
@@ -86,5 +116,51 @@ describe('meetups', () => {
         });
     }); // end of test
   });
+
+  /* Create a question*/
+  describe('POST /api/v1/meetups/:meetupId/questions', () => {
+    it('it should CREATE a question for a specific meetup', (done) => {
+      chai.request(app)
+        .post('/api/v1/meetups/1/questions')
+        .set('access-token', token)
+        .send({
+          title: 'Test Driven Development',
+          body: 'How does Jasmine works ?'
+        })
+        .end((err, res) => {
+          expect(res.status).to.equal(201);
+          expect(Object.keys(res.body.data).length).to.be.above(0);
+          done();
+        });
+    }); // end of test
+  });
+
+  /* Get all questions*/
+  describe('GET /api/v1/meetups/:meetupId/questions', () => {
+    it('it should return an array of questions for a specific meetup', (done) => {
+      chai.request(app)
+        .get('/api/v1/meetups/1/questions')
+        .set('access-token', token)
+        .end((err, res) => {
+          expect(res.status).to.equal(200);
+          expect(res.body.data.length).to.be.above(0);
+          done();
+        });
+    }); // end of test
+  });
+
+  /* Delete a specific meetup */
+  describe('DELETE /api/v1/:meetupId', () => {
+    it('it should delete an existing meetup', (done) => {
+      chai.request(app)
+        .delete('/api/v1/meetups/1')
+        .set('access-token', token)
+        .end((err, res) => {
+          expect(res.status).to.equal(200);
+          expect(res.body.message.toLowerCase()).to.be.equal('meetup deleted');
+          done();
+        });
+    });
+  }); //end of test
 
 });
--- src/tests/users.js
+++ src/tests/users.js
@@ -1,5 +1,6 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
+import db from '../models/db';
 import app from '../app';
 
 const {
@@ -9,19 +10,29 @@ const {
 chai.use(chaiHttp);
 /* Sign-up */
 describe('Sign-up', () => {
+  // clear users table
+  before(async () => {
+    try {
+      await db.query('TRUNCATE users CASCADE; ALTER SEQUENCE users_id_seq RESTART WITH 1;');
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
   describe('POST /api/v1/auth/signup', () => {
     // test 1
     it('should return the information of the created user', (done) => {
       chai.request(app)
         .post('/api/v1/auth/signup')
         .send({
-          firstName: 'Copain',
-          lastName: 'Fabrice',
-          otherName: 'Fabrice',
-          email: 'fabrice@gmail.com',
-          phone: '0789109908',
-          username: 'copain',
-          password: 'copain2018',
+          firstName: 'Jonathan',
+          lastName: 'Rwabahizi',
+          otherName: 'RJ',
+          email: 'jonathanrwabahizi@gmail.com',
+          phone: '+250781146646',
+          username: 'rwajon',
+          password: '12345',
+          isAdmin: true
         })
         .end((err, res) => {
           expect(res.status).to.equal(201);
@@ -34,17 +45,38 @@ describe('Sign-up', () => {
       chai.request(app)
         .post('/api/v1/auth/signup')
         .send({
-          firstName: 'Copain',
-          lastName: 'Fabrice',
-          otherName: 'Fabrice',
-          email: 'fabricegmail.com',
-          phone: '0789109908',
-          username: 'copain',
-          password: 'copain2018',
+          firstName: 'Jonathan',
+          lastName: 'Rwabahizi',
+          otherName: 'RJ',
+          email: 'jonathanrwabahizi-gmail.com',
+          phone: '+250781146646',
+          username: 'rwajon',
+          password: '12345',
+          isAdmin: true
         })
         .end((err, res) => {
           expect(res.status).to.equal(400);
           expect(res.body.error.toLowerCase()).to.be.equal('please enter a valid email address');
+          done();
+        });
+    });
+    // test 3
+    it('should display \'Sorry, this account already exists\'', (done) => {
+      chai.request(app)
+        .post('/api/v1/auth/signup')
+        .send({
+          firstName: 'Jonathan',
+          lastName: 'Rwabahizi',
+          otherName: 'RJ',
+          email: 'jonathanrwabahizi@gmail.com',
+          phone: '+250781146646',
+          username: 'rwajon',
+          password: '12345',
+          isAdmin: true
+        })
+        .end((err, res) => {
+          expect(res.status).to.equal(200);
+          expect(res.body.error.toLowerCase()).to.be.equal('sorry, this account already exists');
           done();
         });
     });
@@ -58,8 +90,8 @@ describe('Sign-in', () => {
       chai.request(app)
         .post('/api/v1/auth/login')
         .send({
-          username: 'copain',
-          password: 'copain2018',
+          email: 'jonathanrwabahizi@gmail.com',
+          password: '12345',
         })
         .end((err, res) => {
           expect(res.status).to.equal(200);
@@ -68,16 +100,30 @@ describe('Sign-in', () => {
         });
     });
     // test 2
-    it('should display \'Please enter valid characters! Only alphabetic characters allowed.\'', (done) => {
+    it('should display \'Please enter a valid email address.\'', (done) => {
       chai.request(app)
         .post('/api/v1/auth/login')
         .send({
-          username: 'copain',
-          password: '',
+          email: 'jonathanrwabahizi-gmail.com',
+          password: '12345',
         })
         .end((err, res) => {
           expect(res.status).to.equal(400);
-          expect(res.body.error.toLowerCase()).to.be.equal('user not found!');
+          expect(res.body.error.toLowerCase()).to.be.equal('please enter a valid email address');
+          done();
+        });
+    });
+    // test 2
+    it('should display \'Sorry, your username or password is incorrect\'', (done) => {
+      chai.request(app)
+        .post('/api/v1/auth/login')
+        .send({
+          email: 'jonathanrwabahizi@gmail.com',
+          password: '11111',
+        })
+        .end((err, res) => {
+          expect(res.status).to.equal(400);
+          expect(res.body.error.toLowerCase()).to.be.equal('sorry, your username or password is incorrect');
           done();
         });
     });
